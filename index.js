7ef5430970aef51f34ca01a0a80566b20ec5fc4d
snaveed1 <159036395+snaveed1@users.noreply.github.com>
2024-10-16 17:19:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ const port = 3001;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from app190\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+        <html>
+            <head>
+                <style>
+                    body {
+                        background-color: lightblue;
+                        font-size: 24px;
+                        font-weight: bold;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                        margin: 0;
+                    }
+                </style>
+            </head>
+            <body>
+                Hello World from app1
+            </body>
+        </html>
+    `);
 });
 
 server.listen(port, hostname, () => {
